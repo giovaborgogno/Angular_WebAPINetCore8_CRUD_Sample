a701e12ca6767556362bafaa70f4cbf9ef9c3c2f
Francisco Giovanni Borgogno <giovaborgogno@live.com.ar>
2024-09-30 21:50:12 +0000

    Updated environment.ts

--- EmployeeCrudAngular/src/environments/environment.ts
+++ EmployeeCrudAngular/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:7150/api/Employee'  // URL de la API para desarrollo
+  apiUrl: 'https://giovaborgogno-crud-backend-qa.azurewebsites.net/api/Employee'  // URL de la API para desarrollo
 };
 
 
